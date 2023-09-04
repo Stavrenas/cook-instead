7d4126e8dba938baa5be5e486ec324134f690306
Stavros Malakoudis <stavrina25@gmail.com>
2023-09-05 00:51:58 +0300

    Add font, center all title texts

--- lib/widgets/recipe_short_widget.dart
+++ lib/widgets/recipe_short_widget.dart
@@ -1,23 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../domain/recipe.dart';
 
 class RecipeShortWidget extends StatelessWidget {
-// The Recipe object to display
   final Recipe recipe;
 
-// Constructor that takes the Recipe object as an argument
   RecipeShortWidget({required this.recipe});
 
 // A helper method that returns a list of widgets for the tags
   List<Widget> _buildTags() {
-// Create an empty list of widgets
     List<Widget> widgets = [];
-
-// Loop through the tags list in the recipe object
     for (String tag in recipe.tags) {
-// Add a Chip widget for each tag with some padding and styling
       widgets.add(
         Padding(
           padding: const EdgeInsets.all(4.0),
@@ -36,13 +29,11 @@ class RecipeShortWidget extends StatelessWidget {
       );
     }
 
-// Return the list of widgets
     return widgets;
   }
 
   @override
   Widget build(BuildContext context) {
-    var imageDims = 150.0;
     var padding = 16.0;
     var elevation = 4.0;
     return Card(
@@ -50,14 +41,18 @@ class RecipeShortWidget extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: padding, vertical: padding),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
+                alignment: Alignment.center,
                 color:
                     Color(int.parse(recipe.color, radix: 16)).withOpacity(0.5),
-                child: Row(children: [
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
                   if (recipe.imagesUrls.isNotEmpty) ...[
-                    Row(children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
                       Image.network(recipe.imagesUrls[0],
                           height: (MediaQuery.of(context).size.width) / 2 -
                               padding -
@@ -65,7 +60,8 @@ class RecipeShortWidget extends StatelessWidget {
                           width: (MediaQuery.of(context).size.width) / 2 -
                               padding -
                               elevation),
-                      SizedBox(
+                      Container(
+                        alignment: Alignment.center,
                         width: (MediaQuery.of(context).size.width) / 2 -
                             padding -
                             elevation,
@@ -76,7 +72,7 @@ class RecipeShortWidget extends StatelessWidget {
                       )
                     ]),
                   ] else ...[
-                    TitleText(recipe.title)
+                    TitleText(recipe.title),
                   ],
                 ])),
             Divider(),
@@ -94,9 +90,9 @@ class RecipeShortWidget extends StatelessWidget {
     return Text(
       title,
       style: TextStyle(
-        fontSize: 24.0,
+        fontSize: 30.0,
+        fontFamily: 'Jetskisinsummer',
         fontWeight: FontWeight.bold,
-        fontFamily: 'Comic Sans MS',
         color: Colors.white,
       ),
       textAlign: TextAlign.center,
